731affb71197fb4323d6618e3c86f613b39cc2c9
Narinder Dev <narinder.dev@gmail.com>
2026-01-12 21:42:35 +0530

    update main.py

--- app/main.py
+++ app/main.py
@@ -9,7 +9,11 @@ app = FastAPI(title="FastAPI Backend")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://64.225.59.206"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://64.225.59.206",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
